449020f0946d5989b2632e174bd998cb691df33b
TyomaKirakos <evolutioner.tyoma@yandex.ru>
2022-12-22 23:45:35 +0300

    finished the main page

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,37 @@
-let swiper = new Swiper(".mySwiper", {
+let slillsSlider = new Swiper(".skills-slider", {
     slidesPerView: 1,
     spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+    breakpoints: {
+        795: {
+            slidesPerView: 2,
+            spaceBetween: 30, },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 30, },
+    }
+});
+
+let awardsSlider = new Swiper(".awards-slider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
     // pagination: {
     //     el: ".swiper-pagination",
     //     clickable: true,
@@ -9,4 +40,12 @@ let swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        1000: {
+            slidesPerView: 2,
+            spaceBetween: 30, },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 30, },
+    }
 });
